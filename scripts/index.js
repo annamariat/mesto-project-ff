83087702293ed21e170364e75f7ec88ee821ceab
Anna Vakhrusheva <Leravalera71288@yandex.ru>
2025-11-13 07:34:26 +0900

    Для консультации добавлены изменения

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,41 @@
 // @todo: Темплейт карточки
-const container = document.querySelector('.content');
-const songsContainer = container.querySelector('.places__list');
-const userTemplate = document.querySelector('#card-template').content;
-const userElement = userTemplate.querySelector('.card-template').cloneNode(true);
-
-
+const cardTemplate = document.querySelector("#card-template").content;
 
 // @todo: DOM узлы
+const placesList = document.querySelector(".places_list");
+const deleteIcon = cardElement.querySelector(".card__delete-icon");
 
 // @todo: Функция создания карточки
+function createCardElement(cardData, deleteCallback) {
+  const cardTemplate = document.querySelector("#card-template").content;
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+
+  cardElement.querySelector(".card__title").textContent = cardData.title;
+  cardElement.querySelector(".card__description").textContent =
+    cardData.description;
+  cardElement.querySelector(".card__image").src = cardData.imageUrl;
+
+  const deleteIcon = cardElement.querySelector(".card__delete-icon");
+  deleteIcon.addEventListener("click", deleteCallback);
+
+  return cardElement;
+}
 
 // @todo: Функция удаления карточки
 
+function deleteCard(cardElement) {
+  cardElement.remove();
+}
+
+initialCards.forEach((cardData) => {
+  const cardElement = createCardElement(cardData, deleteCard);
+  placesList.append(cardElement);
+});
+
 // @todo: Вывести карточки на страницу
+
+initialCards.forEach((cardData) => {
+  const cardElement = createCardElement(cardData, deleteCallback);
+
+  placesList.append(cardElement);
+});
